d69fb0e57bed24a2aef8e5c86239d4248d6a970b
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-20 01:35:25 +0800

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,9 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    if models.storage_type == 'db':
-        id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime(), nullable=False, default=datetime.now)
-        updated_at = Column(DateTime(), nullable=False, default=datetime.now)
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
          
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
