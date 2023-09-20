33d91a81cb0aa570c9ecc002339959bde56edcb5
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-20 23:53:23 +0800

    Update place.py

--- models/place.py
+++ models/place.py
@@ -16,13 +16,13 @@ place_amenity = Table('place_amenity', Base.metadata,
                              ForeignKey('places.id',
                                         onupdate='CASCADE',
                                         ondelete='CASCADE'),
-                                 primary_key=True, nullable=False),
+                             primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
-                              ForeignKey('amenities.id',
+                             ForeignKey('amenities.id',
                                          onupdate='CASCADE',
                                          ondelete='CASCADE'),
-                                 primary_key=True, nullable=False)
-                     )
+                             primary_key=True, nullable=False)
+                       )
 
 
 class Place(BaseModel, Base):
