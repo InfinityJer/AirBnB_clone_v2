2497934bf92cd62de1d18beaa9dc0147a622521f
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-09 23:35:49 +0800

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,53 +5,51 @@ This script uploads a web_static archive to a web server, uncompresses it,
 and creates a symbolic link to the new release.
 """
 
-from os.path import exists
-from fabric.api import put, run, env
+from datetime import datetime
+from fabric.api import *
+import os
 
 env.hosts = ['18.207.2.67', '52.91.120.49']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
 
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+def do_pack():
     """
-    if exists(archive_path) is False:
-        return False
-    try:
-        # Upload the archive to /tmp/
-        put(archive_path, '/tmp/')
-        # Create target filename without extension
-        file_name_ext = archive_path.split("/")[-1]
-        tar_file_name = file_name_ext.split(".")[0]
-        run('sudo mkdir -p /data/web_static/releases/{}/'.
-            format(tar_file_name))
-        # Uncompress archive and delete archive from web server
-        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
-            format(file_name_ext, tar_file_name))
-        run('sudo rm /tmp/{}'.format(file_name_ext))
-        # Move content into host web_static
-        cmd_mv = 'sudo mv /data/web_static/releases/{}/web_static/*' \
-                 ' /data/web_static/releases/{}/'.format(tar_file_name,
-                                                         tar_file_name)
-        run(cmd_mv)
-        # Remove redundant content folder
-        cmd_rm_dir = 'sudo rm -rf /data/web_static/releases/{}/web_static'.\
-            format(tar_file_name)
-        run(cmd_rm_dir)
-        # Delete existing symbolic link
-        run('sudo rm -rf /data/web_static/current')
-        # Reestablish symbolic link with new content folder
-        sym_link = 'sudo ln -s /data/web_static/releases/{}/' \
-                   ' /data/web_static/current'.\
-            format(tar_file_name)
-        run(sym_link)
-        print('New version deployed!')
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
         return True
-    except Exception:
-        return False
+
+    return False
