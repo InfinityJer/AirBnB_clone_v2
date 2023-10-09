94ae93c50ce28f194bc9f3e6d711c42eb5d6cd38
InfinityJer <jclaris6@gmail.com>
2023-10-09 07:39:17 -0700

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+My AirBnB clone repository's web_static folder's 
+contents were used by a Fabric script to create a.tgz archive.
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """Generates archive file from contents of Clone repo"""
+    dt = datetime.utcnow()
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                              dt.month,
+                                                              dt.day,
+                                                              dt.hour,
+                                                              dt.minute,
+                                                              dt.second)
+    if isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return file_name
+
